1ab17a0d0a09fa0f91d29fa491bce135abc32a56
ping.sec <ping@cancloud.com>
2025-09-09 13:30:54 +0700

    update - add client stream

--- protogen/go/hello/hello.pb.go
+++ protogen/go/hello/hello.pb.go
@@ -126,10 +126,11 @@ const file_proto_hello_hello_proto_rawDesc = "" +
 	"\x04name\x18\x01 \x01(\tR\x04name\x12\x10\n" +
 	"\x03age\x18\x10 \x01(\rR\x03age\"%\n" +
 	"\rHelloResponse\x12\x14\n" +
-	"\x05greet\x18\x01 \x01(\tR\x05greet2\x87\x01\n" +
+	"\x05greet\x18\x01 \x01(\tR\x05greet2\xcc\x01\n" +
 	"\fHelloService\x127\n" +
 	"\bSayHello\x12\x13.hello.HelloRequest\x1a\x14.hello.HelloResponse\"\x00\x12>\n" +
-	"\rSayManyHellos\x12\x13.hello.HelloRequest\x1a\x14.hello.HelloResponse\"\x000\x01B,Z*github.com/unknopu/proto/protogen/go/hellob\x06proto3"
+	"\rSayManyHellos\x12\x13.hello.HelloRequest\x1a\x14.hello.HelloResponse\"\x000\x01\x12C\n" +
+	"\x12SayHelloToEveryOne\x12\x13.hello.HelloRequest\x1a\x14.hello.HelloResponse\"\x00(\x01B,Z*github.com/unknopu/proto/protogen/go/hellob\x06proto3"
 
 var (
 	file_proto_hello_hello_proto_rawDescOnce sync.Once
@@ -151,10 +152,12 @@ var file_proto_hello_hello_proto_goTypes = []any{
 var file_proto_hello_hello_proto_depIdxs = []int32{
 	0, // 0: hello.HelloService.SayHello:input_type -> hello.HelloRequest
 	0, // 1: hello.HelloService.SayManyHellos:input_type -> hello.HelloRequest
-	1, // 2: hello.HelloService.SayHello:output_type -> hello.HelloResponse
-	1, // 3: hello.HelloService.SayManyHellos:output_type -> hello.HelloResponse
-	2, // [2:4] is the sub-list for method output_type
-	0, // [0:2] is the sub-list for method input_type
+	0, // 2: hello.HelloService.SayHelloToEveryOne:input_type -> hello.HelloRequest
+	1, // 3: hello.HelloService.SayHello:output_type -> hello.HelloResponse
+	1, // 4: hello.HelloService.SayManyHellos:output_type -> hello.HelloResponse
+	1, // 5: hello.HelloService.SayHelloToEveryOne:output_type -> hello.HelloResponse
+	3, // [3:6] is the sub-list for method output_type
+	0, // [0:3] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
